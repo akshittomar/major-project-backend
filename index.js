984de1caf17494bba0603d982ddd4bb7b07302fb
Akshit Tomar <akshitt125@gmail.com>
2024-05-17 13:13:27 +0530

    Major Project Backend

---
+++ index.js
@@ -0,0 +1,22 @@
+const connection = require('./db');
+const express = require('express');
+var cors = require('cors')
+const auth = require('./routes/createuser')
+const crop = require('./routes/crop')
+connection();
+const app = express();
+const port = 3000;
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.use('/api/auth',auth)
+app.use('/api/crop',crop)
+const server = app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+module.exports = server ;
